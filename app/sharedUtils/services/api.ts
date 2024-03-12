f5354c6fe6043aad14a85226d6802398f8c799b7
Nayan Patel <nayanp@softwareco.com>
2024-03-12 10:34:26 +0530

    Fix : Major lib correction

--- app/sharedUtils/services/api.ts
+++ app/sharedUtils/services/api.ts
@@ -1,102 +1,29 @@
-import Config from 'react-native-config';
+import Config from "react-native-config";
 
-const PMS_BASE_URL: string | any = Config.PMS_BASE_URL;
 const BASE_URL: string | any = Config.BASE_URL;
-
-export const isLoggedInRequest = async () => {
-  return new Promise((resolve, reject) => {
-    let url = `${PMS_BASE_URL}auth/isLoggedIn`;
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      redirect: 'follow',
-    })
-      .then(response => response.json())
-      .then(responseJson => {
-        resolve(responseJson);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-};
-
-export const loginRequest = async (email: string, password: string) => {
-  return new Promise((resolve, reject) => {
-    let url = `${PMS_BASE_URL}auth/login`;
-
-    const raw = JSON.stringify({
-      email: email,
-      password: password,
-    });
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: raw,
-      redirect: 'follow',
-    })
-      .then(response => response.json())
-      .then(responseJson => {
-        resolve(responseJson);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-};
-
-export const getTokenRequest = async (_id: string, clientId: string) => {
-  return new Promise((resolve, reject) => {
-    let url = `${BASE_URL}get-token?userId=${_id}&clientId=${clientId}`;
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      redirect: 'follow',
-    })
-      .then(response => response.json())
-      .then(responseJson => {
-        resolve(responseJson);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-};
 
 export const getChannelListRequest = async (_id: string, token: string) => {
   return new Promise((resolve, reject) => {
     let url = `${BASE_URL}channel/chat-channel?skip=0&limit=10&userId=${_id}`;
 
     console.log(
-      'getChannelListRequest',
-      `${BASE_URL}channel/chat-channel?skip=0&limit=10&userId=${_id}`,
+      "getChannelListRequest",
+      `${BASE_URL}channel/chat-channel?skip=0&limit=10&userId=${_id}`
     );
     fetch(url, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
         Authorization: `Bearer ${token}`,
       },
-      redirect: 'follow',
+      redirect: "follow",
     })
-      .then(response => response.json())
-      .then(responseJson => {
+      .then((response) => response.json())
+      .then((responseJson) => {
         resolve(responseJson);
       })
-      .catch(error => {
+      .catch((error) => {
         reject(error);
       });
   });
@@ -104,25 +31,25 @@ export const getChannelListRequest = async (_id: string, token: string) => {
 
 export const getMessageListRequest = async (
   channelId: string,
-  token: string,
+  token: string
 ) => {
   return new Promise((resolve, reject) => {
     let url = `${BASE_URL}messages?channelId=${channelId}`;
 
     fetch(url, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
         Authorization: `Bearer ${token}`,
       },
-      redirect: 'follow',
+      redirect: "follow",
     })
-      .then(response => response.json())
-      .then(responseJson => {
+      .then((response) => response.json())
+      .then((responseJson) => {
         resolve(responseJson);
       })
-      .catch(error => {
+      .catch((error) => {
         reject(error);
       });
   });
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-export * from "./app/context";
-// export * from "./app/navigation";
+export * from "./app/components/index.ts";
+export * from "./app/context/index.ts";
 export * from "./app/navigation/index.ts";
-
-// export { default as ChannelListScreen } from './app/navigation/ChannelListScreen';
-// export { default as MessageListScreen } from './app/navigation/MessageListScreen';
 
